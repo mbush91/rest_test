88b6fafdce3c726a563c72f5d9287f154ba22aa0
Mike Bush <mike.bush@tomesoftware.com>
2023-02-21 16:11:08 -0500

    t

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import logging
+
+logging.basicConfig(filename="/app/test.log")
+
+log = logging.getLogger("TESTAPP")
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+	log.info(__name__)
 	return "Hello World!"
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
